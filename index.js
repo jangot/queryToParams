bfd45c046398e6c13b07eb61c2ced174924354e3
jangot <pulin.pavel@gmail.com>
2013-08-05 12:56:05 +0400

    Parse all

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var parseQuery = require('./parseQuery');
 
 
-var URL = 'q=query+%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B0&' +
-    'oq=query+%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B0&' +
-    'aqs=chrome.0.69i57j69i62.3929j0&' +
-    'sourceid=chrome&' +
-    'ie=UTF-8' +
-    '&foo[]=2' +
-    '&foo[]=3';
+var URL = 'ff=1'+
+    '&ff[]=2' +
+    '&ff[iii][]=4'+
+    '&ff[iii][]=3'+
+    '&ff[iii][]=gf'+
+    '&q=query+%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B0'+
+    '&oq=query+%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B0' +
+    '&aqs=chrome.0.69i57j69i62.3929j0' +
+    '&sourceid=chrome' +
+    '&ie=UTF-8' +
+    '&foo[]=45' +
+    '&foo=2' +
+    '&foo[fff][hh][eee]=3' +
+    '&foo[fff][hh][eee]=3' +
+    '&foo=4' +
+    '&doo[fff]=6' +
+    '&doo[fff][]=1' +
+    '&doo=2' +
+    '&doo=3';
 
 
 var params = parseQuery(URL);
--- parseQuery.js
+++ parseQuery.js
@@ -10,42 +10,104 @@ module.exports = function(queryString) {
 }
 
 function Params() {
-    this._data = {}
 }
 Params.prototype = {
-    _data : null,
+
+    /**
+     * Keep all params
+     * @type Object
+     */
+    _data : undefined,
+    /**
+     * Add param
+     *
+     * @param key string
+     * @param value any
+     */
     add : function(key, value) {
-        if (/\[\d?\]$/.test(key)) {
-            this._addArray(key, value);
-        } else if (/\[\D+\]$/.test(key)) {
-            this._addObjectKey(key, value);
-        } else if(this._data[key] != undefined) {
-            this._addExist(key, value);
+        var path = key.split(/\[/);
+
+        for (var i = 1; i < path.length; i++) {
+            path[i] = path[i].slice(0, path[i].length-1)
+        }
+
+        this._data = this._addItem(path, this._data, value);
+    },
+
+    /**
+     * Get params object
+     *
+     * @returns {Object|_data}
+     */
+    getParams : function() {
+        return this._data || {};
+    },
+
+    _addItem : function(path, parent, value) {
+        var key = path.shift();
+        if (parent === undefined) {
+            parent = this._getItemByKey(key);
+        }
+        if(path[0]) {
+            parent[key] = this._addItem(path, parent[key], value);
         } else {
-            this._addSimple(key, value);
+            parent = this._addByInstance(parent, key, value);
+        }
+        return parent;
+    },
+
+    _getItemByKey : function(key) {
+        var numberKey = Number(key);
+        if (!isNaN(numberKey) || key == '') {
+            return [];
+        } else {
+            return {};
         }
     },
 
-    getParams : function() {
-        return this._data;
+    _addByInstance : function(object, key, value) {
+        if(object instanceof Array) {
+            object = this._addToArray(object, key, value);
+        } else if (object instanceof Object) {
+            object = this._addToObject(object, key, value);
+        } else if (typeof object == 'string') {
+            object = this._addToArray([object], key, value);
+        }
+        return object;
     },
 
-    _addSimple : function(key, value) {
-        this._data[key] = value;
+    _addToArray : function(array, key, value) {
+        var numberKey = Number(key);
+        var result = array;
+        if (key == '') {
+            result.push(value);
+        } else if (isNaN(numberKey)) {
+            result = {};
+            for (var i = 0; i < array.length; i++) {
+                result[i] = array[i];
+                result[key] = value;
+            }
+        } else {
+            if (array[numberKey]) {
+                result[numberKey] = this._addByInstance(array[numberKey], '', value);
+            } else {
+                result[numberKey] = value;
+            }
+        }
+
+        return result;
     },
 
-    _addArray : function(key, value) {
-       key = key.slice(0, key.length - 2);
-       this._data[key] = this._data[key] || [];
-
-        this._data[key].push(value);
-    },
-
-    _addObjectKey : function() {
-
-    },
-
-    _addExist : function(key, value) {
-
+    _addToObject : function(object, key, value) {
+        if (!key) {
+            key = Object.keys(object).length;
+        }
+        if (object[key]) {
+            object[key] = this._addByInstance(object[key], '', value);
+        } else {
+            object[key] = value;
+        }
+        return object;
     }
+
 }
