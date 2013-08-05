92a1816fdd37164785c0f796048acf9df997cb89
jangot <pulin.pavel@gmail.com>
2013-08-05 14:14:31 +0400

    Parse all

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 var parseQuery = require('./parseQuery');
 
+var URL = 'test=Tooo&gest=uuuu&goo[test1]=1&goo[test2]=2&goo[room][0]=table&goo[room][1]=bad&zoom[]sss=a&zoom=b&zoom[3][ram]=c';
 
-var URL = 'ff=1'+
-    '&ff[]=2' +
-    '&ff[iii][]=4'+
-    '&ff[iii][]=3'+
-    '&ff[iii][]=gf'+
-    '&q=query+%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B0'+
-    '&oq=query+%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B0' +
-    '&aqs=chrome.0.69i57j69i62.3929j0' +
-    '&sourceid=chrome' +
-    '&ie=UTF-8' +
-    '&foo[]=45' +
-    '&foo=2' +
-    '&foo[fff][hh][eee]=3' +
-    '&foo[fff][hh][eee]=3' +
-    '&foo=4' +
-    '&doo[fff]=6' +
-    '&doo[fff][]=1' +
-    '&doo=2' +
-    '&doo=3';
-
-
-var params = parseQuery(URL);
-
-console.log(params);
+console.log(parseQuery(URL));
